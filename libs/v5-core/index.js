7812c42d11daa785ea31a351bc94c1c183808a0f
skyouo0727 <anwugame@gmail.com>
2021-09-13 20:16:21 +0800

    [Core/Minor] Indent

--- libs/v5-core/index.js
+++ libs/v5-core/index.js
@@ -1,6 +1,6 @@
 // original: discord-player (modified by NCT skyouo)
 // License: MIT (See License)
-//Based on discord-player is 2.3.1.
+// Based on discord-player is 2.3.1.
 
 module.exports = {
     version: require('./package.json').version,

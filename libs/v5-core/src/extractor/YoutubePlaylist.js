fba693998630e3ffe60ed7e503bb141b39e92d9a
skyouo0727 <anwugame@gmail.com>
2021-10-05 23:32:58 +0800

    [Core/Patch] Playlist fix

--- libs/v5-core/src/extractor/YoutubePlaylist.js
+++ libs/v5-core/src/extractor/YoutubePlaylist.js
@@ -1,10 +1,11 @@
 const BaseExtractor = require("./BaseExtractor")
+
+const LocalTools = require("../LocalTools");
 const Track = require("../Track");
 const Util = require("../Util");
 
 const YouTube = require('simple-youtube-api');
-const ytsr = require('youtube-sr').default
-const ytpl = require('ytpl')
+const ytpl = require('ytpl');
 
 module.exports = class YoutubeVideoKeywordExtractor extends BaseExtractor {
     constructor(options) {
@@ -17,15 +18,23 @@ module.exports = class YoutubeVideoKeywordExtractor extends BaseExtractor {
             const playlistID = await ytpl.getPlaylistID(query).catch(() => { })
             if (options.useAPI) {
                 let youtube = options.apiKEYs?.length ? new YouTube(options.apiKEYs[Math.floor(Math.random() * options.apiKEYs.length)]) : null
-                const playlist = await youtube.getPlaylistByID(playlistID).catch(() => { })
+                const playlist = await youtube.getPlaylistByID(playlistID).then(playlist => playlist.fetch()).catch(() => { })
                 if (playlist) {
                     const videos = await playlist.getVideos()
                     return await Promise.all(videos.map(async (v) => {
-                        const a = await v.fetch()
-                        v.duration = Util.buildTimecode(a.duration)
-                        return v
+                        try {
+                            const a = await v.fetch()
+                            v.duration = Util.buildTimecode(a.duration)
+                            return v
+                        } catch (e) {
+                            if (v.duration) {
+                                return v
+                            } else {
+                                return null
+                            }
+                        }
                     })).then((result) => {
-                        resolve(result.map((i) => new Track({
+                        resolve(result.filter(v => !!v).map((i) => new Track({
                             title: i.title,
                             duration: i.duration,
                             bestThumbnail: i.maxRes,
@@ -39,12 +48,12 @@ module.exports = class YoutubeVideoKeywordExtractor extends BaseExtractor {
                     resolve([])
                 }
             } else {
-                const playlist = await ytsr.getPlaylist(`https://www.youtube.com/playlist?list=${playlistID}`)
-                return resolve(playlist.videos.map(v => new Track({
+                const playlist = await ytpl(`https://www.youtube.com/playlist?list=${playlistID}`, { gl: 'TW', hl: 'zh', limit: Infinity })
+                return resolve(playlist.items.map(v => new Track({
                     title: v.title,
-                    duration: v.durationFormatted,
+                    duration: LocalTools.ms2mmss(LocalTools.s2ms(v.durationSec)),
                     bestThumbnail: { url: `https://i.ytimg.com/vi/${v.id}/maxresdefault.jpg` },
-                    author: v.channel,
+                    author: v.author,
                     url: `https://www.youtube.com/watch?v=${v.id}`,
                     fromPlaylist: true,
                     fromYoutube: true
